1620d8f27893041d53893719c91e225a03980243
Kevin <kevincasal08@gmail.com>
2016-10-11 17:12:27 +0200

    Este archivo es la primera version hecha 17.12

---
+++ Empresa2/src/empresa2/empresa2Class.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empresa2;
+
+/**
+ *
+ * @author dawbio
+ * the class in the future include function in afect the empresaClass
+ * 
+ */
+public class empresa2Class {
+
+    /**
+     * nombre, string, name of empresa
+     */
+    public String nombre ="DAWBIO";
+    
+    
+}
---
+++ Empresa2/src/empresa2/empresa2Principal.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empresa2;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Kevin Casanova Armada
+ * Is the corp of menu in the Principal, is formate for two optionc add and list all the employee and exit
+ */
+public class empresa2Principal {
+    public static Scanner scanner =new Scanner(System.in);
+    public static ArrayList listaPersonal = new ArrayList();
+ 
+    public static void main(String [] args ) 
+	{
+            int fin=0;
+            while(fin!=3){
+            menu();
+            fin=scanner.nextInt();
+            switch(fin)
+            {
+                case 1: 
+                insertar();
+		break;
+                case 2:
+		listar();
+		break;
+                case 3:
+                System.out.println("Saliste");
+                break;
+                default :
+                System.out.println("Error");
+                break;
+                }
+        }
+    
+}   /**
+ * Is a void the print a menu of the application
+ */
+    public static void menu(){
+        empresa2Class empr = new empresa2Class();
+	System.out.println("Menu de la empresa"+empr.nombre);
+        System.out.println("1.Insertar empleado");
+	System.out.println("2.Listar plantilla");
+	System.out.println("3.Salir");
+    }
+
+    /**
+     * 
+     * insertar, inserta datos, contiene la variable comple que son los dineros del complemento
+     * is a limitet for 10 employee
+     */
+    public static void insertar(){
+        int comple,limit=0;
+        if (limit<=10){
+        empresa2EmpleadoClass emple = new empresa2EmpleadoClass();
+            System.out.println("Nombre");
+            emple.setNombre(scanner.next());
+            System.out.println("Complemento");
+            comple=scanner.nextInt();
+            emple.setSueldo(emple.complemento(comple));
+            listaPersonal.add(emple);
+            limit++;
+        }
+        
+    }
+    
+   /**
+    * listar ,lista los datos
+    */
+    public static void listar(){
+        for( int i=0;i<listaPersonal.size();i++)
+            {
+		empresa2EmpleadoClass emple  = (empresa2EmpleadoClass) listaPersonal.get(i);
+		System.out.println("Nombre: "+emple.getNombre());
+		System.out.println("Sueldo: "+emple.getSueldo());
+            }
+    }
+
+
+}
